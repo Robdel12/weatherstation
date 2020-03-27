13c746fd884bf5acc2f754344eab1411fb821406
Robert DeLuca <robertdeluca19@gmail.com>
2020-03-26 23:26:16 -0500

    useHooks for live component

--- app/src/routes/live.js
+++ app/src/routes/live.js
@@ -1,13 +1,34 @@
-import React, { Component, createRef } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '@material-ui/core/Card';
-import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
-import Loading from '../components/loading';
 
 import OpacityIcon from '@material-ui/icons/Opacity';
+
+function avgCollection(collection) {
+  let sum = collection.reduce(
+    (acc, cv, i, src) => {
+      acc.temp += cv.temp;
+      acc.pressure += cv.pressure;
+      acc.humidity += cv.humidity;
+
+      return acc;
+    },
+    {
+      temp: 0,
+      pressure: 0,
+      humidity: 0
+    }
+  );
+
+  return {
+    temp: sum.temp / collection.length,
+    pressure: sum.pressure / collection.length,
+    humidity: sum.humidity / collection.length
+  };
+}
 
 let styles = {
   container: {
@@ -22,113 +43,72 @@ let styles = {
   }
 };
 
-let $heading = createRef();
+function focusRef(node) {
+  node?.focus();
+}
 
-class LiveFeed extends Component {
-  state = {
-    collection: [],
-    rawData: {
-      temp: 0,
-      pressure: 0,
-      humidity: 0,
-      currentWindDirection: '',
-      currentWindSpeed: 0
-    }
-  };
+function Live({ classes }) {
+  let [collection, updateCollection] = useState([]);
+  let [rawData, updateRawData] = useState({
+    temp: 0,
+    pressure: 0,
+    humidity: 0,
+    currentWindDirection: '',
+    currentWindSpeed: 0
+  });
 
-  hasLoaded() {
-    if ($heading.current) {
-      $heading.current.focus();
-    }
-  }
-
-  updateWeather = ({ data }) => {
+  function updateWeather({ data }) {
     let parsedData = JSON.parse(data);
-    let { collection } = this.state;
+    updateRawData(parsedData);
 
     // Keep 15 recods around to avg (about 30 seconds of data)
     if (collection.length > 15) {
       collection = collection.slice(0, 14);
     }
 
-    collection.push(parsedData);
-    this.setState({ collection, rawData: parsedData });
-  };
+    collection = collection.concat(parsedData);
+    updateCollection(collection);
+  }
 
-  componentDidMount() {
+  useEffect(() => {
     let protocol = window.location.protocol === 'https' ? 'wss://' : 'ws://';
     let hasPort = !!window.location.port ? `:${window.location.port}` : '';
     let hostname = `${window.location.hostname}${hasPort}`;
+    // let socket = new WebSocket(`ws://weather.deluca.house/v1`);
+    let socket = new WebSocket(`${protocol}${hostname}/v1`);
 
-    this.socket = new WebSocket(`${protocol}${hostname}/v1`);
-    this.socket.addEventListener('message', this.updateWeather);
-  }
+    socket.addEventListener('message', updateWeather);
+    return () => socket.close();
+  }, []);
 
-  componentWillUnmount() {
-    this.socket.close();
-  }
+  let data = collection.length < 3 ? rawData : avgCollection(collection);
 
-  avgCollection() {
-    let { collection, rawData } = this.state;
+  return (
+    <div className={classes.container} data-test-live-route>
+      <Typography variant="h3" component="h1" gutterBottom className={classes.title}>
+        <span tabIndex={-1} ref={focusRef}>
+          Live weather
+        </span>
+      </Typography>
 
-    if (collection.length < 3) {
-      return rawData;
-    }
-
-    let sum = collection.reduce(
-      (acc, cv, i, src) => {
-        acc.temp += cv.temp;
-        acc.pressure += cv.pressure;
-        acc.humidity += cv.humidity;
-
-        return acc;
-      },
-      {
-        temp: 0,
-        pressure: 0,
-        humidity: 0
-      }
-    );
-
-    return {
-      temp: sum.temp / collection.length,
-      pressure: sum.pressure / collection.length,
-      humidity: sum.humidity / collection.length
-    };
-  }
-
-  render() {
-    let { rawData } = this.state;
-    let { classes } = this.props;
-    let data = this.avgCollection();
-
-    return (
-      <div className={classes.container} data-test-live-route>
-        <Typography variant="h3" component="h1" gutterBottom className={classes.title}>
-          <span tabIndex={-1} ref={$heading}>
-            Live weather
-          </span>
-        </Typography>
-
-        <Card data-test-live-card>
-          <CardContent>
-            <Typography variant="h4" gutterBottom data-test-temp>
-              {parseInt(data.temp, 10)} F
-            </Typography>
-            <Typography variant="body1" gutterBottom data-test-humidity>
-              <OpacityIcon /> {parseInt(data.humidity, 10)}%
-            </Typography>
-            <Typography variant="body1" gutterBottom data-test-pressure>
-              {parseInt(data.pressure, 10)} hPa
-            </Typography>
-            <Typography variant="body1" gutterBottom data-test-wind>
-              {rawData.currentWindSpeed.toFixed(2)} mph ({rawData.currentWindDirection})
-            </Typography>
-          </CardContent>
-        </Card>
-      </div>
-    );
-  }
+      <Card data-test-live-card>
+        <CardContent>
+          <Typography variant="h4" gutterBottom data-test-temp>
+            {parseInt(data.temp, 10)} F
+          </Typography>
+          <Typography variant="body1" gutterBottom data-test-humidity>
+            <OpacityIcon /> {parseInt(data.humidity, 10)}%
+          </Typography>
+          <Typography variant="body1" gutterBottom data-test-pressure>
+            {parseInt(data.pressure, 10)} hPa
+          </Typography>
+          <Typography variant="body1" gutterBottom data-test-wind>
+            {rawData.currentWindSpeed.toFixed(2)} mph ({rawData.currentWindDirection})
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
-export default withStyles(styles)(LiveFeed);
+export default withStyles(styles)(Live);
