0ced72b862c657e82fdebacc880c6017915b7bbd
Robert DeLuca <robertdeluca19@gmail.com>
2021-11-04 23:16:16 -0500

    ♻️🏗 Remove Karma tests, use Cypress
    This is because Parcel isn't really going to work well with Karma. As much as I
    love Karma, I don't want to sink my time into making it work well with
    Parcel. The wins from using Cypress is these are E2E tests hitting the local
    server (I've been trolled by dep updates in the past because of the mocked API)
    and it makes us make interactor.js work with Cypress.

---
+++ app/cypress/integration/app.test.js
@@ -0,0 +1,15 @@
+describe("application smoke test", () => {
+  it("renders home without error", () => {
+    cy.visit("http://localhost:8080");
+    cy.get("h3.text-3xl").then(($h3) => {
+      expect($h3).to.have.text("Highs & lows");
+    });
+  });
+
+  it("renders live without error", () => {
+    cy.visit("http://localhost:8080/live");
+    cy.get("h1").then(($h1) => {
+      expect($h1).to.have.text("Live weather");
+    });
+  });
+});
--- app/test/index.js
+++
@@ -1,9 +0,0 @@
-require('@babel/polyfill');
-
-mocha.timeout(3000);
-
-const requireComponentTests = require.context('../src', true, /\.test$/);
-const requireAcceptanceTests = require.context('.', true, /\.test$/);
-
-requireComponentTests.keys().forEach(requireComponentTests);
-requireAcceptanceTests.keys().forEach(requireAcceptanceTests);
